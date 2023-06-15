33305899e1826f4d2e390b7df691bd28af4c641f
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-15 17:56:27 +0000

    fixed error on 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,13 @@ int main(void)
 	char c;
 	int i;
 	float f;
+	long int li;
+	long long int lli;
 
 	printf("Size of a char: %lu byte(S)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (long)sizeof(i));
-	printf("Size of a long long int: %lu byte(s)\n", (long-long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
