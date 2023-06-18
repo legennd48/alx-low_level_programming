51e52106f1bdd7be2c2bfc84f320524fa28a020c
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-18 20:20:35 +0000

    changed a few things, used a ternary statement in line 21 to ensure 10s are no longer skipped

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,14 +18,14 @@ int main(void)
 		{
 			for (d3 = d1; d3 <= '9'; d3++)
 			{
-				for (d4 = d2 + 1 ; d4 <= '9'; d4++)
+				for (d4 = (d3 == d1 ? d2 + 1 : '0'); d4 <= '9';  d4++)
 				{
 					putchar(d1);
 					putchar(d2);
 					putchar(' ');
 					putchar(d3);
 					putchar(d4);
-					if (!((d1 == '9' && d2 == '8') && (d4 == '9' && d3 == '9')))
+					if (!(d1 == '9' && d2 == '8' && d3 == '9' && d4 == '9'))
 					{
 						putchar(',');
 						putchar(' ');
