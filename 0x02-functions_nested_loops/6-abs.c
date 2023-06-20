d8786163294a73d21c29671496ac04c194ecebc6
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-21 00:33:10 +0100

    added 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolite value of a number
+ * @n: number to be compited
+ * Return: 0
+ */
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	if (n < 0)
+	{
+		return (-n);
+	}
+}
