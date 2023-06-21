6a8620f6a434fb9db1a413fc80269dc7fc325483
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-21 01:25:17 +0000

    added 9 times...

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *times_table - prints the 9th times table starting from 0
+ */
+void times_table(void)
+{
+	int mult;
+	int row;
+	int col;
+
+	for (row = 0; row < 10; row ++)
+	{
+		for (col = 0; col < 10; col++)
+		{
+			mult = (col * row);
+
+			if (col == 0)
+			{
+				_putchar('0' + mult);
+			}
+			else if (mult < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + mult);
+			}
+			else if (mult > 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + mult);
+			}
+		}
+		_putchar('\n');
+	}
+}
