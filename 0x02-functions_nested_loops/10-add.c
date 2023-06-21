7e5522159af818aac097cb208387aed01b4b0221
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-21 02:08:48 +0000

    added 10..

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - adds 2 numbers
+ *@x: one of the numbers to be added
+ *@y: the seccond number ti be added
+ *Return: 0
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
