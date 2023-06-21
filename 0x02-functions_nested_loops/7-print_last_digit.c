3021319830899d55aa8083dedba83680fc7067f2
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-21 01:00:51 +0100

    ADDED 7...

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number to be examined.
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int m = (n % 10);
+
+	_putchar('0' + m);
+	return (m);
+}
+
