28ecc035dbee819c017b38b84a5691ed9d33b54d
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-21 00:56:25 +0000

    added 8-24...

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute for 24 hours
+ */
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -38,7 +38,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-
+void jack_bauer(void);
 
 
 
