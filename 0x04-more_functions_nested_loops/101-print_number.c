55c6ea98aff307f1f318683950fe6b203ebdcb9c
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-23 07:43:49 +0000

    redone 101...

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,5 +7,32 @@
 
 void print_number(int n)
 {
-	_putchar('0' + n);
+	unsigned int digs, tens, absolute_n, control;
+
+	absolute_n = n;
+	control = 1;
+	if (n > 0 || n < 0)
+	{
+		if (n < 0)
+		{
+			absolute_n = (n * -1);
+			_putchar('-');
+		}
+
+		while (control <= absolute_n)
+			control = (control * 10);
+		tens = (control / 10);
+
+		while (tens >= 1)
+		{
+			digs = (absolute_n / tens);
+			_putchar(digs + '0');
+			absolute_n = (absolute_n - (tens * digs));
+			tens = (tens / 10);
+		}
+	}
+	else
+	{
+		_putchar ('0');
+	}
 }
