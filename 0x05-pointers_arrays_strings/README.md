b98d034e6e80340914355c43cbc8cbfe31d79bda
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-26 16:41:10 +0000

    made dir, README, main.h and 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+This dir was created to help me learn of pointers, arrays and strings in c
+it contains a list of all files and source i created at the time of learning and
+their desc.
+thanks
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *reset_to_98 - takes a pointer to an int as parameter and updates the value it
+ *points to to 98
+ *@*n: the pointer being taken
+ */
+void reset_to_98(int *m);
+
+
+
+
+
+#endif
