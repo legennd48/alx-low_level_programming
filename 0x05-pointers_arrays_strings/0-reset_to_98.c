c7c9427d0ec46276b1b47fb931386dfbc9a3af94
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-26 16:46:10 +0000

    fixed betty style for 0-reset...

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  *reset_to_98 - takes a pointer to an int as parameter and updates the value
  * it points to to 98
- *@*n: the pointer taken to be reset to 98
+ *@n: the pointer taken to be reset to 98
  */
 
 void reset_to_98(int *n)
