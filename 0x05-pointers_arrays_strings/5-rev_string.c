a91a1f408e178b17fa8bb9143da656c2a7d94d09
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-26 23:06:17 +0000

    used diff method on 5...

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,34 +1,23 @@
 #include "main.h"
 
 /**
-*rev_string - reverses a string without std libs
-*@s: string to be reversed
-*/
-
+ * rev_string - reverses a string without std libs
+ * @s: string to be reversed
+ */
 void rev_string(char *s)
 {
 	int lent = 0, i;
-	char temp[1000];
-	char *x = temp;
+	char temp;
 
-	while (*s != '\0')
+	while (s[lent] != '\0')
 	{
-		s++;
 		lent++;
 	}
-	if (*s == '\0')
-	{
-		s--;
-		lent--;
-	}
 
-	for (i = lent; i >= 0; i--)
+	for (i = 0; i < lent / 2; i++)
 	{
-		*(x + i) = *(s - i);
-	}
-
-	for (i = 0; i <= lent; i++)
-	{
-		*(s + i) = *(x + i);
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
 	}
 }
