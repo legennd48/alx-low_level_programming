91d77395f2264e9d9f35155d01c181a723b8288d
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-26 19:00:02 +0000

    just saving progress

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -16,7 +16,13 @@ void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
-
+/**
+ *_strlen - reads the lenght of a string
+ *@s: string whose lenght is to be read
+ *
+ *Return: lenght of string
+ */
+int _strlen(char *s);
 
 
 #endif
