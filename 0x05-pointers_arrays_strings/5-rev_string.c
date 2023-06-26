9db576d09fdf6e11aa5ba6c4396df158d3f8dcfe
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-26 23:28:34 +0100

    added 5...

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*rev_string - reverses a string without std libs
+*@s: string to be reversed
+*/
+
+void rev_string(char *s)
+{
+	int lent = 0, i;
+	while (*s != '\0')
+	{
+		s++;
+		lent++;
+	}
+	if (*s == '\0')
+	{
+		s--;
+		lent--;
+	}
+	char temp[1000];
+	char *x = temp;
+
+	for (i = lent; i >= 0; i--)
+	{
+		*(x + i) = *(s - i);
+	}
+	*s = *x;
+}
