e5747e64d8b7d30813b7562d985a412cfd451e2c
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-27 03:12:51 +0000

    added 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - generates random valid passwords for the program 101-crackme.
+ *Return: 0
+ */
+
+int main(void)
+{
+	int password[100];
+	int i, total, n;
+
+	total = 0;
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		password[i] = rand() % 78;
+		total += (password[i] + '0');
+		putchar(password[i] + '0');
+
+		if ((2772 - total) - '0' < 78)
+		{
+			n = 2772 - total - '0';
+			total += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return 0;
+}
