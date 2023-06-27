67cc5103ce63f89c3ddc6bb32a5b0b5e116469b1
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-27 00:49:00 +0000

    added 8-print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - prints the n elements of an array of integers followed
+ *a be new line
+ *@a: array to be printed from
+ *@n: number of elemnts to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		if (i == 0)
+		{
+			printf("%d, ", *a);
+		}
+		else if ((i > 0) && (i < n - 1))
+		{
+			printf("%d, ", *(a + i));
+		}
+		else
+		{
+			printf("%d\n", *(a + i));
+		}
+	}
+}
