396f82cbf3638727a750808ee541ea928b8e7fb5
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-29 23:09:06 +0000

    fixed error on 2...

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int c = 0;
+
 	while (c < n && src[c] != '\0')
 	{
 		dest[c] = src[c];
 		c++;
 	}
-	dest[c] = '\0';
+	while (c < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
 
 	return (dest);
 }
