5d61e40746f97f070a06581138bc43a8c21e3045
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-06-30 00:39:15 +0000

    modified 100..

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,22 @@
 
 char *rot13(char *s)
 {
+	int i, j;
+
 	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int i;
 
-	while (*s != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; i < 52; i++)
+		for (j = 0; alpha[j] != '\0'; j++)
 		{
-			if (*s == alpha[i])
+			if (s[i] == alpha[j])
 			{
-				*s = rot[i];
+				s[i] = rot[j];
 				break;
 			}
 		}
-	s++;
 	}
+
 	return (s);
 }
