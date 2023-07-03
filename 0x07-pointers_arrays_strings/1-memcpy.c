fd3f227c4175d5d2d14d72f6f5e601d0210e7bbb
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-03 11:43:35 +0000

    fixed error in 1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,10 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (dest[i] != '\0' && src[i] != '\0')
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];
 	}
 	return (dest);
 }
