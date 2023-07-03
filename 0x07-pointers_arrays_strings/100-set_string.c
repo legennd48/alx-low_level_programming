dc534bdd2a9513e19ffcba0742b09f0723f3551b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-04 00:30:58 +0100

    added 100...

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *set_string - sets thr value of a ptr to a char
+ *@s: pointer to be set.
+ *@to: ehat it is to be set to
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
