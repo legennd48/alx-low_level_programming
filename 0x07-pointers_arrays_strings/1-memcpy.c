0827a5bb3985e336542f680cb180be12fb159e54
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-03 11:34:51 +0000

    modifird 1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,7 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (src[i] != '\0')
+		if (dest[i] != '\0' && src[i] != '\0')
 		{
 			dest[i] = src[i];
 		}
