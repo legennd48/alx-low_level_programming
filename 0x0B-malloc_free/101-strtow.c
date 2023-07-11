369b700455fdae8072a14e1e5ea2d7cbcc8e1b84
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-11 21:28:44 +0000

    fixed a mistake in 101...

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,30 +12,25 @@ char **strtow(char *str)
 	int wcount, i, j, pos;
 	char **words;
 
-	if (str == NULL || str[0] == '\0')
+	if (str == NULL || str[0] == '\0' || str == "")
 		return (NULL);
-
 	wcount = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 			wcount++;
 	}
-
 	words = malloc(sizeof(char *) * (wcount + 1));
 	if (words == NULL)
 		return (NULL);
-
 	pos = 0;
 	for (i = 0; i < wcount; i++)
 	{
 		while (str[pos] == ' ')
 			pos++;
-
 		j = pos;
 		while (str[j] != ' ' && str[j] != '\0')
 			j++;
-
 		words[i] = malloc(sizeof(char) * (j - pos + 1));
 		if (words[i] == NULL)
 		{
@@ -44,14 +39,11 @@ char **strtow(char *str)
 			free(words);
 			return (NULL);
 		}
-
 		for (j = 0; str[pos] != ' ' && str[pos] != '\0'; j++, pos++)
 			words[i][j] = str[pos];
 
 		words[i][j] = '\0';
 	}
-
 	words[wcount] = NULL;
-
 	return (words);
 }
