5c8f85c8ca7910c11a143aac729692d8962e90b2
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-13 16:12:15 +0000

    added new file

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenes all of string and n bytes of second string
+ * to a new dynamically allocated pointer
+ * @s2: second string
+ * @s1: first string
+ * @n: number of bytes to be copied from second string
+ * Return: Null or pointer to new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new;
+	unsigned int i = 0, j = 0, k;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	if (n >= j)
+		new = malloc(sizeof(char) * (j + i));
+	else
+		new = malloc(sizeof(char) * (i + n + 1));
+	if (new == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		new[k] = s1[k];
+	if (n < j)
+		for (k = 0; k < n; k++)
+		{
+			new[i] = s2[k];
+			i++;
+		}
+	else
+	{
+		k = 0;
+		for (k = 0; s2[k] != '\0'; k++)
+		{
+			new[i] = s2[k];
+			i++;
+		}
+	}
+	new[i] = '\0';
+	return (new);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: number of bytes to be alocated
+ * Return: pointer to allocated memory ot NULL
+ */
+
 void *malloc_checked(unsigned int b);
 
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 
 
