6fc84eebc13de4beba682031c763060546bcd915
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-17 16:08:48 +0000

    betty fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,8 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-/**	if (d->age <= 0)
+/*
+ *	if (d->age <= 0)
  *		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
  *	else
  */
