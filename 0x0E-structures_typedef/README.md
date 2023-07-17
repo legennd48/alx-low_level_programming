fb5286302221bee39098385945bae40206df33e2
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-17 14:07:30 +0000

    initial commit with file 0 and .gitignore

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+this dir was made to help me learn all i can about strustures and typedefs
+you will find a list of files and what they do
+enjoy!
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Structure for dog data
+ * @name: pet name
+ * @age: age in dog years
+ * @owner: name of owner in string
+ *
+ * Description:  made to store data of dogs from all across the petniverse
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif
