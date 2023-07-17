a34aaaef0dc256d080b0e213acd5d86af327fc6e
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-17 14:30:38 +0000

    added new file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to data to be initialised
+ * @name: name of pet
+ * @age: age of pet
+ * @owner: name of pet owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,13 @@ struct dog
 	char *owner;
 };
 
-
+/**
+* init_dog - initializes a variable of type struct dog
+* @d: pointer to data to be initialised
+* @name: name of pet
+* @age: age of pet
+* @owner: name of pet owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
