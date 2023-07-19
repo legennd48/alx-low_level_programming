8c74bd7d153389fb17035bd7321b0610fbe96088
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-19 14:54:19 +0000

    added file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,12 @@
 #include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to functions used for printing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,9 @@
 #ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+
+
+#endif
