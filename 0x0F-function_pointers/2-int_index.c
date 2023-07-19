989f9a2b09ec69cf203d541dc14c9e5a0b6759f2
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-19 16:42:46 +0000

    debugged 2-..

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,21 +6,22 @@
  * @array: array to be searched
  * @size: size of array
  * @cmp: pointer to searching function
+ * Return: -1 when no match found, i if found
  */
 
 int int_index(int *array, int size,  int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (i == size)
-			return (-1);
+/*
+ *		if (i == size)
+ *			return (-1);
+ */
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
