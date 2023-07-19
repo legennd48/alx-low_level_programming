d3e8477ba176942a13da0129b538f6e8fadae163
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-19 23:31:49 +0000

    added 101

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: argument vector - number of bytes to be printed
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	char *e = "Error", *ar = (char *)main;
+	int bytes, i;
+
+	if (argc != 2)
+	{
+		printf("%s\n", e);
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("%s\n", e);
+		exit(2);
+	}
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == (bytes - 1))
+		{
+			printf("%02hhx ", ar[i]);
+			break;
+		}
+		printf("%02hhx ", ar[i]);
+	}
+	return (0);
+}
