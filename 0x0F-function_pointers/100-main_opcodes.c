89d43d804351c59fe0260ec90df923ca918e7ab4
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-20 06:48:17 +0000

    debugged 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-
 
 /**
  * main - prints its own opcodes
@@ -9,40 +7,30 @@
  * @argv: argument vector - number of bytes to be printed
  * Return: 0 success
  */
-
 int main(int argc, char *argv[])
 {
 	char *e = "Error";
-	int (*ar)(int, char**) = main;
 	int bytes, i, main_bytes = 0;
+	unsigned char *main_ptr;
 
 	if (argc != 2)
 	{
 		printf("%s\n", e);
 		exit(1);
 	}
+
 	bytes = atoi(argv[1]);
 
-	if (bytes < 0)
+	if (bytes <= 0)
 	{
 		printf("%s\n", e);
 		exit(2);
 	}
-	while (main_bytes < bytes)
-	{
-		if (((char *)ar)[main_bytes] == '\0')
-			break;
-		main_bytes++;
-	}
 
-	for (i = 0; i < bytes && i < main_bytes; i++)
+	main_ptr = (unsigned char *)main;
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == (bytes - 1) || i == (main_bytes - 1))
-		{
-			printf("%.2x ", *((unsigned char *)(ar + i)));
-			break;
-		}
-		printf("%.2x ", *((unsigned char *)(ar + i)));
+		printf("%.2x ", main_ptr[i]);
 	}
 	printf("\n");
 	return (0);
