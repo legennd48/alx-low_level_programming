26cbaad576bbf36651393cace23a207d0efaf009
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-20 10:32:39 +0000

    added new file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @seperator: string that separates each printed number
+ * @n: number of arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list list;
+
+	if (n == 0)
+		return;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(list, int);
+
+		if (i == (n - 1))
+		{
+			printf("%d\n", num);
+			return;
+		}
+		if (separator != NULL)
+			printf("%d%s", num, separator);
+		else
+			printf("%d", num);
+	}
+}
