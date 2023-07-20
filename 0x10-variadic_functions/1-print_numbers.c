a41429666b1e72601afc263e8d461d5ce9f7335b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-20 10:53:08 +0000

    debugged 1 and 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -33,4 +33,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		else
 			printf("%d", num);
 	}
+	va_end(list);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -35,4 +35,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", string);
 	}
+	va_end(strings);
 }
