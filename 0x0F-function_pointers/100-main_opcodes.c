63a0b4fc94fb65cf6c8f9c97fc78ad67f435442f
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-20 07:02:35 +0000

    changes logic

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,8 +10,8 @@
 int main(int argc, char *argv[])
 {
 	char *e = "Error";
-	int bytes, i, main_bytes = 0;
-	unsigned char *main_ptr;
+	int bytes, i;/*main_bytes = 0;*/
+	/*unsigned char *main_ptr;*/
 
 	if (argc != 2)
 	{
@@ -21,17 +21,33 @@ int main(int argc, char *argv[])
 
 	bytes = atoi(argv[1]);
 
-	if (bytes <= 0)
+	if (bytes < 0)
 	{
 		printf("%s\n", e);
 		exit(2);
 	}
 
-	main_ptr = (unsigned char *)main;
+	/*main_ptr = (unsigned char *)main;*/
 	for (i = 0; i < bytes; i++)
 	{
-		printf("%.2x ", main_ptr[i]);
+		printf("%02hhx", *((char *)main + i));
+		if (i < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
+
+
+
+
+
+/*
+ *  for (i = 0; i < bytes; i++)
+ *	{
+ *		printf("%.2x ", main_ptr[i]);
+ *	}
+ *	printf("\n");
+ *	return (0);
+ */
