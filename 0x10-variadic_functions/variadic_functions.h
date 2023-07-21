f6174d4b738ee1460e412e7fb422aa6cef878b38
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-21 08:47:31 +0100

    fixes structure description

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,16 @@ int sum_them_all(const unsigned int n, ...);
 int _putchar(char);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * f - structure defining data type of format specifier and
+ * tge corresponding function
+ * @type: the format character
+ * @func: function pointer of thr corresponfing function
+ *
+ * Description:this helps to select the function that matches the provided specifier
+ */
+
 typedef struct f
 {
 	char type;
