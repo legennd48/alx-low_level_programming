1b9602da39932673ad57e9c136b379cdee56eefb
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-28 00:00:14 +0100

    debugged Please enter the commit message for your changes. Lines starting

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	unsigned int nodes = 0;
 
 	if (h == NULL)
 		return (0);
@@ -24,7 +24,9 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%u] %s\n", h->len, h->str);
 		}
+
 		nodes++;
+
 		h = h->next;
 	}
 
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@
 void __attribute__((constructor)) message_b4_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf( "I bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
