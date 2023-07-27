f8f2143e89366890c3cc30700fd73692d590df3e
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-27 14:24:53 +0000

    initialized dir and added new file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head ptr to the first node
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nul)\n");
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
