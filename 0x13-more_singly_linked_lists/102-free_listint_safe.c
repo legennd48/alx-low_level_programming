a75688af680eac22e76e37e8278e694002501c81
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-08-01 14:35:20 +0000

    new file

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - frees a listint_t list with a loop
+ * @h: pointer to the head node of the list
+ * Return: number of nodes
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	unsigned int nodes = 0;
+	listint_t *current_node, *temp;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current_node = *h;
+
+	while (current_node != NULL)
+	{
+		nodes++;
+		temp = current_node;
+		current_node = current_node->next;
+
+		/* Set the next pointer of the current node to NULL */
+		temp->next = NULL;
+
+		/* Free the current node */
+		free(temp);
+
+		/* Break the loop if a loop is detected */
+		if (current_node == *h)
+		{
+			*h = NULL;
+			break;
+		}
+	}
+	*h = NULL;
+
+	return (nodes);
+}
