24f67208c8b860d548fdac11f3a6e42c24684ec0
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-15 00:23:07 +0000

    correct logic

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index index of
+ * a dlistint_t linked list.
+ * @head: pointer to pointer to head node
+ * @index: index where the node is to be deleted
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp, *prev;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		return (1);
+	}
+
+	temp = *head;
+	while (temp != NULL && i < index)
+	{
+		prev = temp;
+		temp = temp->next;
+		i++;
+	}
+
+	if (temp == NULL)
+		return (-1);
+
+	prev->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = prev;
+
+
+	return (1);
+}
