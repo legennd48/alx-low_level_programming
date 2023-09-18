1afd45895b2fe9dd619c6367ea23d3c3cc39367c
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-18 18:17:42 +0000

    small fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp "https://github.com/legennd48/alx-low_level_programming/blob/main/0x18-dynamic_libraries/inject.so"
+wget -P /tmp https://github.com/legennd48/alx-low_level_programming/blob/main/0x18-dynamic_libraries/inject.so
 export LD_PRELOAD=/tmp/inject.so
