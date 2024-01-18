663731034734186bc4ae3193a5dd4c74a3366b88
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-18 10:17:27 +0000

    changed loop condition

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,30 +10,35 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low, high, pos;
+    size_t low, high, pos;
 
-	if (!array || size == 0)
-		return (-1);
+    if (!array || size == 0)
+        return (-1);
 
-	low = 0;
-	high = size - 1;
+    low = 0;
+    high = size - 1;
 
-	while (low <= high && value >= array[low] && value <= array[high])
-	{
-		/* Calculate the probable position using interpolation formula */
-		pos = low + ((double)(high - low) /
-			     (array[high] - array[low])) * (value - array[low]);
+    for (low = 0, high = size -1; high >= low;)
+    {
+        /* Calculate the probable position using interpolation formula */
+        pos = low + ((double)(high - low) / (array[high] - array[low])) * (value - array[low]);
 
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+        if (pos >= size)
+        {
+            printf("Value checked array[%ld] is out of range\n", pos);
+	    break;
+        }
 
-		if (array[pos] == value)
-			return (pos);
+        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-		if (array[pos] < value)
-			low = pos + 1;
-		else
-			high = pos - 1;
-	}
+        if (array[pos] == value)
+            return (pos);
 
-	return (-1);
+        if (array[pos] < value)
+            low = pos + 1;
+        else
+            high = pos - 1;
+    }
+
+    return (-1);
 }
